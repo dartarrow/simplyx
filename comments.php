6663f7d8e9c0b499788013b8be6cfd856af9e792
root <root@web.comanche.dartarrow.com>
2014-03-21 10:53:22 -0400

    initial commit

---
+++ comments.php
@@ -0,0 +1,13 @@
+<div id="comments">
+    <div id="fb-root"></div>  
+    <script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>  
+    <fb:comments href="<?php the_permalink(); ?>" width="440"></fb:comments>  
+</div>
+
+<?php 
+/**
+ * ALVIN:
+ * added this to completely switch to FB comments. 
+ * pay attention to the sizes, esp width
+ */
+?>
